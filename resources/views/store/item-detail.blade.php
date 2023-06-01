51befed6968f14aada9896cd97f9da41bf42453e
ThiccPan <ihilmiderian@gmail.com>
2023-06-02 01:52:58 +0700

    update view

--- resources/views/store/item-detail.blade.php
+++ resources/views/store/item-detail.blade.php
@@ -8,20 +8,28 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-@include('layouts.user_navigation')
+    @include('store.component.user_navigation')
 
 <div class="my-24 max-w-7xl mx-auto">
 <div class="flex flex-row justify-between">
     {{$item}}
 </div>
-<form method="POST" action="{{route('user.cart', $item)}}" id="item-to-cart">
-    @csrf
-    <input type="hidden" name="item_id" value="{{$item}}">
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-</form>
 
+<div class="my-8">
+    <form method="POST" action="{{route('user.cart.add', $item)}}" id="item-to-cart">
+        @csrf
+        <div>
+            <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">quantity</label>
+            <input type="number" name="quantity" id="quantity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required min="0" max="{{$item->stock}}">
+        </div>
+        
+        <input type="hidden" name="item_id" value="{{$item}}">
+        <button type="submit" class="disabled text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+</div>
+    
 </div>
 
-  
+@include('store.component.footer')
 </body>
 </html>
