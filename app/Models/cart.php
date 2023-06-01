48082b3a2e5d9d4c868a46685f661dbbecd72bdb
ThiccPan <ihilmiderian@gmail.com>
2023-06-01 20:27:45 +0700

    update cart model and migration properties

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class cart extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'total_price'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
 }
