d292e590e28a10ad535ba8ea3c8e3296da44158e
ThiccPan <ihilmiderian@gmail.com>
2023-06-01 21:06:55 +0700

    controller feature show item detail

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
@@ -84,7 +85,9 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+        return view('store.item-detail', [
+            'item' => $item,
+        ]);
     }
 
     /**
